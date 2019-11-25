365f3c341b643e6f8f40d040b4f1ba466c86b347
杨帆 <yangfan@jianbing.com>
2019-11-25 15:53:41 +0800

    *fix

--- client/src/components/Header/Search.js
+++ client/src/components/Header/Search.js
@@ -3,7 +3,15 @@ import style from './Search.scss'
 export default class Search extends PureComponent {
   render(){
     return (
-      <div className={style.search}></div>
+      <div className={style.search}>
+        <div className={style.searchBox}>
+          <input className={style.searchInput} type="text"/>
+          <span className={`icon-search ${style.searchIcon}`}></span>
+        </div>
+        <div className={style.resultBox}>
+
+        </div>
+      </div>
     )
   }
   
